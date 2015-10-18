4e2ea99fef71347f248d66dffe1bd4f804e48191
Toan Truong <truongtoansgu@gmail.com>
2015-10-18 16:49:19 +0700

    init MVC project

---
+++ SGURestaurant/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SGURestaurant.Startup))]
+namespace SGURestaurant
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
